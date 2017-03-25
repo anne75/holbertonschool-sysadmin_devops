fd1e74bb4c46f987713a2c65ff3593fe42b8579d
Anne <anne.cognet@holbertonschool.com>
2017-03-25 05:25:48 +0000

    change again

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,7 @@
 # There is a typ in a php file so the website returns 500
 exec { 'sed -i s@.phpp @.php @ /var/www/html/wp-settings.ph':
   command => 'sed  -i "s@.phpp@.php@" /var/www/html/wp-settings.php',
-  onlyif  => 'test ! -f /var/www/html/wp-settings.php',
-  path    => ['/usr/bin','/usr/sbin','/bin','/sbin'],
+  path    => ['/bin','/sbin','usr/bin','usr/sbin'],
 }
 
 # There is a typo in a php file so the website returns 500
@@ -11,6 +10,7 @@ exec { 'sed -i s@.phpp @.php @ /var/www/html/wp-settings.ph':
 
 # exec { '/bin/sed -i s@.phpp @.php @ /var/www/html/wp-settings.ph':
 #  command => "/bin/sed  -i \"s@$string1@$string2@\" /var/www/html/wp-settings.php",
+#  onlyif  => 'test ! -f /var/www/html/wp-settings.php',
 # }
 
 # service { 'apache2':
