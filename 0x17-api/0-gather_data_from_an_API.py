89064753820f0059f5d4c57a85bf40664a6f9d13
Anne <anne.cognet@holbertonschool.com>
2017-04-17 23:28:10 +0000

    add check if no user with such id

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -8,16 +8,28 @@ import requests
 import json
 
 
+def get_user_info(userid):
+    """
+    Request info to above api
+
+    Arguments:
+        userid: a user id
+    """
+    name = requests.get("{}/users/{}".format(
+        base_url, sys.argv[1])).json().get("name")
+    if name is None:
+        return
+    r = requests.get("{}/todos?userId={}".format(
+        base_url, sys.argv[1])).json()
+    completed_tasks = [e for e in r if e.get("completed") is True]
+    print("Employee {} is done with tasks({:d}/{:d}):".format(
+        name, len(completed_tasks), len(r)))
+    if completed_tasks:
+        print("\t", end="")
+        print("\n\t".join(e.get("title") for e in completed_tasks))
+
+
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
     if len(sys.argv) > 1:
-        name = requests.get("{}/users/{}".format(
-            base_url, sys.argv[1])).json().get("name")
-        r = requests.get("{}/todos?userId={}".format(
-            base_url, sys.argv[1])).json()
-        completed_tasks = [e for e in r if e.get("completed") is True]
-        print("Employee {} is done with tasks({:d}/{:d}):".format(
-            name, len(completed_tasks), len(r)))
-        if completed_tasks:
-            print("\t", end="")
-            print("\n\t".join(e.get("title") for e in completed_tasks))
+        get_user_info(sys.argv[1])
