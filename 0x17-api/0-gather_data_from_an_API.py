11059c750e779b05c0234f99740a86418273bafa
Anne <anne.cognet@holbertonschool.com>
2017-04-18 00:06:37 +0000

    remove requests, use urllib

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@ Use https://jsonplaceholder.typicode.com/ to request user name and tasks
 from given userID
 """
 import json
-import requests
+import urllib.request
 import sys
 
 
@@ -15,21 +15,28 @@ def get_user_info(userid):
     Arguments:
         userid: a user id
     """
-    name = requests.get("{}/users/{}".format(
-        base_url, sys.argv[1])).json().get("name")
-    if name is None:
-        return
-    r = requests.get("{}/todos?userId={}".format(
-        base_url, sys.argv[1])).json()
-    completed_tasks = [e for e in r if e.get("completed") is True]
-    print("Employee {} is done with tasks({:d}/{:d}):".format(
-        name, len(completed_tasks), len(r)))
-    if completed_tasks:
-        print("\t", end="")
-        print("\n\t".join(e.get("title") for e in completed_tasks))
+    base_url = "https://jsonplaceholder.typicode.com"
+    with urllib.request.urlopen("{}/users/{}".format(
+            base_url, userid)) as f:
+        html = f.read()
+    if html:
+        r = json.loads(html.decode('utf-8'))
+        name = r.get("name")
+        if name is None:
+            return
+        with urllib.request.urlopen("{}/todos?userId={}".format(
+                base_url, userid)) as f:
+            html2 = f.read()
+        if html2:
+            r2 = json.loads(html2.decode('utf-8'))
+            completed_tasks = [e for e in r2 if e.get("completed") is True]
+            print("Employee {} is done with tasks({:d}/{:d}):".format(
+                name, len(completed_tasks), len(r)))
+            if completed_tasks:
+                print("\t", end="")
+                print("\n\t".join(e.get("title") for e in completed_tasks))
 
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com"
     if len(sys.argv) > 1:
         get_user_info(sys.argv[1])
