18026c03e20784ecbeed806c5e40bc1d2e59d4d1
Anne <anne.cognet@holbertonschool.com>
2017-04-18 00:17:42 +0000

    revert to requests

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -3,9 +3,9 @@
 Use https://jsonplaceholder.typicode.com/ to request user name and tasks
 from given userID
 """
-import json
-import urllib.request
 import sys
+import requests
+import json
 
 
 def get_user_info(userid):
@@ -15,34 +15,21 @@ def get_user_info(userid):
     Arguments:
         userid: a user id
     """
-    base_url = "https://jsonplaceholder.typicode.com"
-    try:
-        with urllib.request.urlopen("{}/users/{}".format(
-                base_url, userid)) as f:
-            html = f.read()
-        if html:
-            r = json.loads(html.decode('utf-8'))
-            name = r.get("name")
-            if name is None:
-                return
-            with urllib.request.urlopen("{}/todos?userId={}".format(
-                    base_url, userid)) as f:
-                html2 = f.read()
-                if html2:
-                    r2 = json.loads(html2.decode('utf-8'))
-                    completed_tasks = [e for e in r2 if
-                                       e.get("completed") is True]
-                    print("Employee {} is done with tasks({:d}/{:d}):".
-                          format(
-                              name, len(completed_tasks), len(r)))
-                    if completed_tasks:
-                        print("\t", end="")
-                        print("\n\t".join(e.get("title") for e in
-                                          completed_tasks))
-    except urllib.error.HTTPError:
-        pass
+    name = requests.get("{}/users/{}".format(
+        base_url, sys.argv[1])).json().get("name")
+    if name is None:
+        return
+    r = requests.get("{}/todos?userId={}".format(
+        base_url, sys.argv[1])).json()
+    completed_tasks = [e for e in r if e.get("completed") is True]
+    print("Employee {} is done with tasks({:d}/{:d}):".format(
+        name, len(completed_tasks), len(r)))
+    if completed_tasks:
+        print("\t", end="")
+        print("\n\t".join(e.get("title") for e in completed_tasks))
 
 
 if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com"
     if len(sys.argv) > 1:
         get_user_info(sys.argv[1])
