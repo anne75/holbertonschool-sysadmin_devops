6fa9851a353d3fb790f7f74843635fed082fdea3
Anne <anne.cognet@holbertonschool.com>
2017-04-18 00:13:16 +0000

    make try clause

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -16,25 +16,31 @@ def get_user_info(userid):
         userid: a user id
     """
     base_url = "https://jsonplaceholder.typicode.com"
-    with urllib.request.urlopen("{}/users/{}".format(
-            base_url, userid)) as f:
-        html = f.read()
-    if html:
-        r = json.loads(html.decode('utf-8'))
-        name = r.get("name")
-        if name is None:
-            return
-        with urllib.request.urlopen("{}/todos?userId={}".format(
+    try:
+        with urllib.request.urlopen("{}/users/{}".format(
                 base_url, userid)) as f:
-            html2 = f.read()
-        if html2:
-            r2 = json.loads(html2.decode('utf-8'))
-            completed_tasks = [e for e in r2 if e.get("completed") is True]
-            print("Employee {} is done with tasks({:d}/{:d}):".format(
-                name, len(completed_tasks), len(r)))
-            if completed_tasks:
-                print("\t", end="")
-                print("\n\t".join(e.get("title") for e in completed_tasks))
+            html = f.read()
+        if html:
+            r = json.loads(html.decode('utf-8'))
+            name = r.get("name")
+            if name is None:
+                return
+            with urllib.request.urlopen("{}/todos?userId={}".format(
+                    base_url, userid)) as f:
+                html2 = f.read()
+                if html2:
+                    r2 = json.loads(html2.decode('utf-8'))
+                    completed_tasks = [e for e in r2 if
+                                       e.get("completed") is True]
+                    print("Employee {} is done with tasks({:d}/{:d}):".
+                          format(
+                              name, len(completed_tasks), len(r)))
+                    if completed_tasks:
+                        print("\t", end="")
+                        print("\n\t".join(e.get("title") for e in
+                                          completed_tasks))
+    except urllib.error.HTTPError:
+        pass
 
 
 if __name__ == "__main__":
