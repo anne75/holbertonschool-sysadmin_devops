c4a4768e8387a26ec29a688afac004fefef5980c
Anne <anne.cognet@holbertonschool.com>
2017-05-26 21:32:28 +0000

    path to sed

--- 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # The default configuration file for mginx only allows 15 max open files
 # this triggers errors when the server receives too many requests
 exec {'change limit from 15 to 15000 in /etc/default/nginx':
-  command => 'sed -i "s@ULIMIT=\"-n 15\"@ULIMIT=\"-n 15000\"@" /etc/default/nginx && /etc/init.d/nginx restart',
+  command => '/bin/sed -i "s@ULIMIT=\"-n 15\"@ULIMIT=\"-n 15000\"@" /etc/default/nginx && /etc/init.d/nginx restart',
 }
