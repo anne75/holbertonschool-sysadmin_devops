cbc893edad702e76e4b0e40068de5c888d529050
Anne <anne.cognet@holbertonschool.com>
2017-05-26 20:09:44 +0000

    increase limit

--- 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # The default configuration file for mginx only allows 15 max open files
 # this triggers errors when the server receives too many requests
-exec {'change limit from 15 to 1024 in /etc/default/nginx':
-  command => 'sed -i "s@ULIMIT=\"-n 15\"@ULIMIT=\"-n 1024\"@" /etc/default/nginx && /etc/init.d/nginx restart',
+exec {'change limit from 15 to 2048 in /etc/default/nginx':
+  command => 'sed -i "s@ULIMIT=\"-n 15\"@ULIMIT=\"-n 2048\"@" /etc/default/nginx && /etc/init.d/nginx restart',
 }
