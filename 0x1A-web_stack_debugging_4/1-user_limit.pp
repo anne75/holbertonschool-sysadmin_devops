8ce5e3f82585d9c0e1e41dc56d3ddcddea6df803
Anne <anne.cognet@holbertonschool.com>
2017-05-26 23:23:37 +0000

    fix command

--- 0x1A-web_stack_debugging_4/1-user_limit.pp
+++ 0x1A-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,8 @@
 # grant user holberton better max open files limits
+$command1 = "s@holberton hard nofile 5@holberton hard nofile 1024@"
+$command2 = "s@holberton soft nofile 4@holberton soft nofile 1024@"
 
 exec {'change limits for user in /etc/security/limits.conf':
-  command =>'sed -i "s@holberton hard nofile 5@holberton hard nofile 1024@;\
-s@holberton soft nofile 4@holberton soft nofile 1024@" /etc/security/limits.conf',
+  command =>"sed -i \"${command1}; ${command2}\" /etc/security/limits.conf",
   path    =>['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
 }
