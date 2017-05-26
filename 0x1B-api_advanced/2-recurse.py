f10ecd433997d9170f1870130cf0c0aae0f0cfdb
Anne <anne.cognet@holbertonschool.com>
2017-05-26 20:08:19 +0000

    redo recursion in 1 function

--- 0x1B-api_advanced/2-recurse.py
+++ 0x1B-api_advanced/2-recurse.py
@@ -5,30 +5,25 @@ recurse a request, poor api
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-    """
-    entry point for the recursion
-    """
-    url = "https://www.reddit.com/r/{}.json?limit=100".format(subreddit)
-    h = requests.utils.default_headers()
-    headers = {'User-Agent': "ubuntu:noredditapp (by /u/noone)'"}
-    h.update(headers)
-    return (helper(url, h, "start"))
+base_url = "https://www.reddit.com/r/{}.json?limit=100"
+h = requests.utils.default_headers()
+headers = {'User-Agent': "ubuntu:noredditapp (by /u/noone)'"}
+h.update(headers)
 
 
-def helper(url, headers, after, hot_list=[]):
+def recurse(subreddit, hot_list=[], after="start"):
     """
     the recursion
     """
-    base_url = url
+    url = base_url.format(subreddit)
     if after != "start":
         url += "&after={}".format(after)
-    r = requests.get(url, headers=headers, allow_redirect=False)
+    r = requests.get(url, headers=h, allow_redirects=False)
     if r.status_code != 200:
         return None
     for value in r.json()['data'].get('children', []):
         hot_list.append(value['data']['title'])
-    after = r['data'].get('after')
+    after = r.json()['data'].get('after')
     if not after:
         return hot_list
-    return (helper(base_url, headers, after, hot_list))
+    return (recurse(subreddit, hot_list, after))
