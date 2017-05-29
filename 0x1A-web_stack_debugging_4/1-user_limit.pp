f381a2404112ce3ea42400e05846b3aa3e226931
Anne <anne.cognet@holbertonschool.com>
2017-05-29 00:42:25 +0000

    add 1, make a *

--- 0x1A-web_stack_debugging_4/1-user_limit.pp
+++ 0x1A-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # grant user holberton better max open files limits
-$command1 = 's@holberton hard nofile 5@holberton hard nofile 1024@'
-$command2 = 's@holberton soft nofile 4@holberton soft nofile 1024@'
+$command1 = 's@holberton hard nofile 5@* hard nofile 1024@'
+$command2 = 's@holberton soft nofile 4@* soft nofile 1024@'
 
 exec {'change limits for user in /etc/security/limits.conf':
   command => "sed -i \"${command1}; ${command2}\" /etc/security/limits.conf",
