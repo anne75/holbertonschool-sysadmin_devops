71ae5fc3992e2ed88b4ed488bae6c9ce0afa499f
Anne <anne.cognet@holbertonschool.com>
2017-05-29 00:49:52 +0000

    add 1 try to remove lines

--- 0x1A-web_stack_debugging_4/1-user_limit.pp
+++ 0x1A-web_stack_debugging_4/1-user_limit.pp
@@ -3,6 +3,6 @@ $command1 = 's@holberton hard nofile 5@* hard nofile 1024@'
 $command2 = 's@holberton soft nofile 4@* soft nofile 1024@'
 
 exec {'change limits for user in /etc/security/limits.conf':
-  command => "sed -i \"${command1}; ${command2}\" /etc/security/limits.conf",
+  command => "sed '/holberton/d' /etc/security/limits.conf",
   path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 }
